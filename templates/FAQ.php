f744228c5f86ac818a4198ba48e1e13316d8b1e2
pindonga <brunobian@gmail.com>
2020-07-21 11:08:16 -0300

    esto es para otro proyecto

--- templates/FAQ.php
+++ templates/FAQ.php
@@ -2,30 +2,35 @@
 <html>
 	<head>
 		<title>MORFEMAS</title>
+		<link href="{% static "css/bootstrap.css" %}" rel="stylesheet">
+		<link href="{% static "css/font-awesome.css" %}" rel="stylesheet">
+		<link href="{% static "css/bootstrap-social.css" %}" rel="stylesheet">
+		<link href="{% static "css/signin.css" %}" rel="stylesheet">
 	</head>
 	<body id="main_body" class="container">
-    <div id="form_container">
-        <form class="form-signin" method="get" >
-			<div class="navbar navbar-inverse navbar-static-top fondo" role="navigation"> 
-				<div style="margin-top:5%;margin-bottom:2%;margin-left:10%;margin-right:10%;">
-					<h2 class="form-signin-heading">Diccionario de morfemas (BETA)</h2>
-					<h4>Gracias por utilizar nuestro diccionario de morfemas</h4> 
-					<p>Buscá el morfema (por ahora solo sufijos derivativos) que necesites y obtendrás su frecuencia y las palabras que lo utilizan</p>
-					<p>También podés buscar una determinada palabra y obtener qué sufijo derivativo o pseudosufijo la comforma</p>
-					<br>
-					<div style="margin-bottom:1%">
-						<input class="form-control" name="Sufijo" placeholder="Sufijo"> 
-						<input class="btn btn-lg btn-loggin btn-block" style ="margin-top:20px" type="submit" value="Buscar por sufijo">
-					</div>
+    
+		<div id="form_container">
+			<form class="form-signin" method="get" >
+				<div class="navbar navbar-inverse navbar-static-top fondo" role="navigation"> 
+					<div style="margin-top:5%;margin-bottom:2%;margin-left:10%;margin-right:10%;">
+						<h2 class="form-signin-heading">Diccionario de morfemas (BETA)</h2>
+						<h4>Gracias por utilizar nuestro diccionario de morfemas</h4> 
+						<p>Buscá el morfema (por ahora solo sufijos derivativos) que necesites y obtendrás su frecuencia y las palabras que lo utilizan</p>
+						<p>También podés buscar una determinada palabra y obtener qué sufijo derivativo o pseudosufijo la comforma</p>
+						<br>
+						<div style="margin-bottom:1%">
+							<input class="form-control" name="Sufijo" placeholder="Sufijo"> 
+							<input class="btn btn-lg btn-loggin btn-block" style ="margin-top:20px" type="submit" value="Buscar por sufijo">
+						</div>
 
-					<div style="margin-bottom:1%">
-						<input class="form-control" name="palabra" placeholder="Palabra"> 
-						<input class="btn btn-lg btn-loggin btn-block" style ="margin-top:20px" type="submit" value="Buscar por palabra">
+						<div style="margin-bottom:1%">
+							<input class="form-control" name="palabra" placeholder="Palabra"> 
+							<input class="btn btn-lg btn-loggin btn-block" style ="margin-top:20px" type="submit" value="Buscar por palabra">
+						</div>
 					</div>
 				</div>
-			</div>
-        </form>	
-    </div>	
+			</form>	
+		</div>	
 
 		<?php
 			$dbServername = "localhost";
