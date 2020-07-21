556d7ea027c79ff681c7104e6b1d1b5a50e2df1a
pindonga <brunobian@gmail.com>
2020-07-21 10:16:38 -0300

    esto es para otro proyecto

---
+++ templates/db.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "brunobian";
+$dbPassword = "tusam.vive";
+$dbName = "morfemas";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
