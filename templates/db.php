43111869f1453c04fd1df4b769e75094e1800c2a
pindonga <brunobian@gmail.com>
2020-07-21 10:29:36 -0300

    esto es para otro proyecto

--- templates/db.php
+++ templates/db.php
@@ -6,3 +6,8 @@ $dbPassword = "tusam.vive";
 $dbName = "morfemas";
 
 $conn = pg_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+if (!$conn) {
+  echo "An error occurred.\n";
+  exit;
+}
